b7b0147a198cbee4b96c72b8f93553809c3e5c84
Agu Christopher <aguchris740@gmail.com>
2023-07-08 11:13:11 +0100

    Hello world

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,13 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Word Match App'),
+        ),
+        body: Center(child: Text('Hello world')),
+      ),
+    );
   }
 }
